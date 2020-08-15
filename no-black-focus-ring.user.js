80423264cb0e53cadafaaf1bef0b5cf7fabb05ad
samuvuo <samuvuo@gmail.com>
2020-08-15 15:09:01 +0300

    Create no-black-focus-ring.user.js

---
+++ no-black-focus-ring.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         No black focus ring
+// @namespace    userscript.for.vivaldi
+// @description  Do not show black focus ring (3px wide solid black outline)
+// @include      http*
+// @version      1.0
+// @icon         https://pasteboard.co/Jmtuiky.png
+// ==/UserScript==
+"use strict";
+
+function makeStyle(){
+    const style = document.createElement('style');
+    style.innerHTML = `
+    button:focus, button:focus:after { outline: none !important; }
+    a:focus, a:focus:after, a:visited:focus { outline: none !important; }
+    :focus, :focus-within, :focus-visible, :focus:not(:focus-visible) { outline: none !important; }
+    `;
+    document.body.appendChild(style);
+}
+
+makeStyle();
